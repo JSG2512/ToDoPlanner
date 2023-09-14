c550d1a593e678d4c369cd8b4db84d02c8277f93
JSG2512 <root2512@gmail.com>
2023-09-14 13:53:50 +0900

    DTO

--- src/main/java/com/example/todoplanner/todo/dto/ToDoDTO.java
+++ src/main/java/com/example/todoplanner/todo/dto/ToDoDTO.java
@@ -1,4 +1,23 @@
 package com.example.todoplanner.todo.dto;
 
+import com.example.todoplanner.todo.entity.ToDoEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ToDoDTO {
+    private String id;
+    private String title;
+    private boolean done;
+
+    public ToDoDTO(final ToDoEntity entity) {
+        this.id = id;
+        this.title = title;
+        this.done = done;
+    }
 }
